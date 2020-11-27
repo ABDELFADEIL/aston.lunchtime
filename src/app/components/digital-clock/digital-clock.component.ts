25187454824566bffa29e94a7abb76c489293109
BrunaLins <51920797+BrunaLins@users.noreply.github.com>
2020-11-27 10:54:44 +0100

    commit cheyu2

--- src/app/components/digital-clock/digital-clock.component.ts
+++ src/app/components/digital-clock/digital-clock.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-digital-clock',
   templateUrl: './digital-clock.component.html',
-  styleUrls: ['./digital-clock.component.css']
+  styleUrls: ['./digital-clock.component.scss']
 })
 export class DigitalClockComponent implements OnInit {
 
+  private daysArray =['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche'];
+
+  private date = new Date();
+  public hour : any;
+  public minute: string;
+  public second: string;
+  public ampm: string;
+  public day:string;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit()  {
+    setInterval(()=>{
+      const date = new Date();
+      this.updateDate(date);
+    },1000);
+    this.day= this.daysArray[this.date.getDay()];
   }
+private updateDate(date:Date){
+  const hours = date.getHours();
 
+  this.ampm = hours>=12 ? 'PM' : 'AM';
+  this.hour = hours % 12;
+  this.hour = this.hour ? this.hour:12;
+  this.hour = this.hour <10 ? '0' + this.hour : this.hour;
+
+  const minutes = date.getMinutes();
+  this.minute = minutes <10 ? '0' + minutes: minutes.toString();
+  const seconds = date.getSeconds();
+  this.second = seconds <10 ? '0' + seconds : seconds.toString();
+}
 }
