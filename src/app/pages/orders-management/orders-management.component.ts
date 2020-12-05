935743c18ddd8e6f2b4dd556d5881a1a4160bdb2
Clément Balleur <clementballeur@gmail.com>
2020-12-05 17:37:16 +0100

    creation table order management

--- src/app/pages/orders-management/orders-management.component.ts
+++ src/app/pages/orders-management/orders-management.component.ts
@@ -1,6 +1,5 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
-import {OrderListModule} from 'primeng/orderlist';
 import { OrdersService } from 'src/app/services/orders.service';
 
 @Component({
@@ -10,33 +9,47 @@ import { OrdersService } from 'src/app/services/orders.service';
 })
 export class OrdersManagementComponent implements OnInit {
 
+  listeCommandesDays:any = [];
+  cols: any[];
   constructor(private orderService: OrdersService) {
 
    }
 
   ngOnInit(): void {
-    // this.getAllOrders();
+    // this.getOrders();
     // this.getOrderById(1);
     // this.getOrderByUserId(1);
-    this.getAllOrdersForAllUsersByDate(1,"2020-07-12");
+    this.getAllOrdersForAllUsersByDate(0,"2020-07-12");
+    this.cols = [
+      { field: 'date', header: 'date' },
+      { field: 'name', header: 'Name' },
+      { field: 'category', header: 'Category' },
+      { field: 'quantity', header: 'Quantity' }
+  ];
+    // this.getAllOrdersForAllUsersByDate(0,"2020-07-12");
   }
 //recuperer toutes les commandes
   async getOrders() {
    const allOrders = await  this.orderService.getOrders()
+   console.log("getOrders =>");
    console.log(allOrders);
   }
   //recuperer une commande avec un id
   async getOrderById(id:number) {
    const order = await  this.orderService.getOrderById(id)
+   console.log("getOrderById =>");
    console.log(order);
   }
   async getOrderByUserId(id:number) {
    const userOrders = await  this.orderService.getOrderByUserId(id)
+   console.log("getOrderByUserId =>");
    console.log(userOrders);
   }
   async getAllOrdersForAllUsersByDate(status?:number,beginDate?:string,endDate?:string) {
    const allOrdersByDate = await  this.orderService.getAllOrdersForAllUsersByDate(status,beginDate,endDate)
+   this.listeCommandesDays = allOrdersByDate
+   console.log("getAllOrdersForAllUsersByDate =>");
    console.log(allOrdersByDate);
   }
-
+  
 }
