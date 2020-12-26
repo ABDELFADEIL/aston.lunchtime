7fce5eb88fb52c81bef3e69703b6890e82d866c2
ABDELFADEIL <abdalfadeil@gmail.com>
2020-12-26 18:14:40 +0100

    pagination meals page

--- src/app/pages/meal/meal.component.ts
+++ src/app/pages/meal/meal.component.ts
@@ -17,7 +17,7 @@ export class MealComponent implements OnInit {
   cols: any[];
   public meal: any;
   page: number = 1;
-  currentPage:number;
+  currentPage:number = this.page;
   meals:any[]=[];
   pages: any[]=[];
 
@@ -38,7 +38,6 @@ export class MealComponent implements OnInit {
   async getAllMeals() {
     const response = await this.mealService.getMeals();
     this.allMeals = response;
-    console.log(response);
     this.allMeals.forEach(element => {
       this.getMealImage(element.id);
     });
@@ -47,11 +46,9 @@ export class MealComponent implements OnInit {
 
     async getMealImage(id) {
       const res = await this.mealService.findImgMeal(id);
-      console.log(res);
       this.allMeals.forEach(element=>{
         if(element.imageId === res.id){
           element.img64 = res.image64;
-
         }
       })
       /*    const img = res.image64;
@@ -59,11 +56,18 @@ export class MealComponent implements OnInit {
     }
 
     paginateMeals(page_number) {
+      console.log(page_number)
+
       const totalPages:any= this.allMeals.length/12;
-      console.log(totalPages)
-      console.log(this.allMeals.length/12);
       this.pages= new Array<number>(parseInt(totalPages)+1);
-      this.meals = this.allMeals.slice((page_number-1) * 12, page_number * 12);
+      if (0 > page_number){
+        this.currentPage = 0;
+      } else if (page_number > this.pages.length) {
+        this.currentPage == this.pages.length - 1;
+      } else {
+        this.currentPage = page_number;
+      }
+      this.meals = this.allMeals.slice((this.currentPage-1) * 12, this.currentPage * 12);
       return this.meals;
 
     }
