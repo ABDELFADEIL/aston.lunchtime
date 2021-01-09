1303078c8cc7452fc907e316d275aebdff9ed090
Clément Balleur <clementballeur@gmail.com>
2021-01-09 17:43:09 +0100

    ajout d'un message lors de la validation du panier et clean du code order component

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MealService } from 'src/app/services/meal-service.service';
 import { OrdersService } from 'src/app/services/orders.service';
-import {Order} from "../../models/order";
+import { Order } from "../../models/order";
 
 @Component({
   selector: 'app-order',
@@ -12,27 +12,36 @@ export class OrderComponent implements OnInit {
 
 
   list: any[] = [];
+  messageInfo: string;
   constructor(public orderService: OrdersService, public mealService: MealService) { }
 
   ngOnInit(): void {
+    this.messageInfo = undefined;
     this.getMealList();
-    // this.list = this.mealService.mealList
-    // console.log("MALISTE =>");
-    // console.log(this.mealService.mealList);
   }
 
+  /////////////////////////////////////// FONCTIONS DU PANIER ///////////////////////////////////////
+
+
+  /**
+   * validation de la commande
+   */
   async commanderHo() {
     console.log(this.orderService.order);
     return await this.orderService.addOrder(this.orderService.order)
       .then(res => {
         console.log("res", res);
         this.orderService.order = new Order();
+        this.messageInfo = "Votre commande a bien été prise en compte !"
       })
       .catch(err => {
         console.log("err", err);
       });
   }
-
+  /**
+   * retirer une unité d'un plat
+   * @param id 
+   */
   retirerPlat(id) {
     let item = this.orderService.order.quantity[id]
     if (item.quantity > 1) {
@@ -41,26 +50,31 @@ export class OrderComponent implements OnInit {
       this.orderService.order.quantity.splice(id, 1);
     }
   }
+  /**
+   * vider le panier
+   */
   emptyCart() {
     this.orderService.order.quantity = [];
   }
+  /**
+   * recuperer le label du plat affiché
+   * @param mealId 
+   */
   getLabel(mealId) {
-    // console.log("MEALID =>")
-    // console.log(mealId)
-    // console.log(this.list)
     for (let meal of this.list) {
       if (meal.id === mealId) {
-        console.log("TTTTTTTTT")
         return meal.label;
       }
     }
   }
-
+  /**
+   * recuperer la liste des plats
+   */
   getMealList() {
-     this.mealService.getMealWeek().then( res => {
-       this.list = res;
-       console.log("MEAL LIST = >")
-       console.log(this.list)
-     })
+    this.mealService.getMealWeek().then(res => {
+      this.list = res;
+      console.log("MEAL LIST = >")
+      console.log(this.list)
+    })
   }
 }
