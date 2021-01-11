848161a81b515c707eeebef91a9b41f9d879c29e
ABDELFADEIL <abdalfadeil@gmail.com>
2021-01-11 22:43:44 +0100

    url remote

--- src/app/api-url/url.ts
+++ src/app/api-url/url.ts
@@ -1,3 +1,3 @@
 
-export const URL = "http://localhost:8080/lunchtime/"
+export const URL = "http://ec2-18-198-97-101.eu-central-1.compute.amazonaws.com:8080/lunchtime/"
 //http://ec2-18-198-97-101.eu-central-1.compute.amazonaws.com:8080/lunchtime/";
